c1c8c93dcd484914486d409811cdb3977dbba9b7
sano <70003587+sano-jin@users.noreply.github.com>
2020-09-04 16:52:03 +0900

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,4 +1,6 @@
-
+import requests
+import json
+import os
 from picamera import PiCamera
 from time import sleep
 
@@ -11,3 +13,14 @@ sleep(3)
 camera.capture('/home/pi/image.jpg')
 
 camera.stop_preview()
+
+files = {'file': open("/home/pi/image.jpg", 'rb')}
+param = {
+    'token':TOKEN, 
+    'channels':CHANNEL,
+    'filename':"filename",
+    'initial_comment': "initial_comment",
+    'title': "title"
+}
+
+requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
