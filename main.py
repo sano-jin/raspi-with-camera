6cbdbb3c51b65d8129b33cadd81fd44dc7b98f82
sano <70003587+sano-jin@users.noreply.github.com>
2020-09-04 18:50:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,12 @@ TOKEN =	#
 CHANNEL = #                                                                             
 
 camera = PiCamera()
-
 camera.start_preview()
-
 sleep(3)
-
 camera.capture('/home/pi/image.jpg')
-
 camera.stop_preview()
 
 files = {'file' : open('/home/pi/image.jpg', 'rb')}
-param = {
-    'token' : TOKEN,
-    'channels': CHANNEL
-    }
-
-r_post = requests.post("https://slack.com/api/files.upload",
-                       params = param,
-		       files = files)
-
+param = {'token' : TOKEN, 'channels': CHANNEL}
+r_post = requests.post("https://slack.com/api/files.upload", params = param, files = files)
 # print( r_post.json() ) 
